d7b8fd34a8b448ac85cee2e14e88da9780afb2f9
somercic1 <somercic1@etf.unsa.ba>
2020-11-05 00:46:20 +0100

    testni commit

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -3,4 +3,15 @@ package ba.unsa.etf.rpr.tutorijal02;
 public class Interval {
 
 
+    public Interval(double v, double v1, boolean b, boolean b1) {
+    }
+
+    public boolean isIn(double v) {
+    }
+
+    public boolean isNull() {
+    }
+
+    public Interval intersect(Interval interval) {
+    }
 }
